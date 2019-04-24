cc38a85aac2e67e9fca92f3bea30693a2bde789f
Gault, Chris <chris.gault@tsi.com>
2019-04-24 10:38:59 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module source.developers.google.com/p/dj-build/r/helloworld
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"source.developers.google.com/p/dj-build/r/sandy"
+)
+
+func main() {
+	sandy.SayHello()
+	// fmt.Printf("hello world\n")
+}
